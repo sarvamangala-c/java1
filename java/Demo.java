a94916f4a1a95ec6ba2ca75cfbdee8fa7a654fbc
sarvamangala-c <sarvamangala123456789@gmail.com>
2025-04-23 16:42:53 +0530

    Update Demo.java

--- java/Demo.java
+++ java/Demo.java
@@ -5,6 +5,10 @@ class Demo
         int b=5;
         int sum =a+b;
         int sub =a-b;
+        int mul =a*b;
+        int div =a/b;
+        System.out.println("mul is :"+mul);
+        System.out.println("div is :"+div);
         System.out.println("sum is :"+sum);
         System.out.println("sub is :"+sub);
     }
