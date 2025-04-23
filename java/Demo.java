35b0573ee7c1b12fbf3d8b1f51bb97f7e0287f36
sarvamangala-c <sarvamangala123456789@gmail.com>
2025-04-23 15:23:12 +0530

    save

---
+++ java/Demo.java
@@ -0,0 +1,11 @@
+class Demo
+{
+    public static void main(String[] args) {
+        int a=20;
+        int b=5;
+        int sum =a+b;
+        int sub =a-b;
+        System.out.println("sum is :"+sum);
+        System.out.println("sub is :"+sub);
+    }
+}
